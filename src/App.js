eac8a7d9a78be006823d43df4f6b660bea05ea03
Dominggus11 <roymalau12304@gmail.com>
2022-09-17 15:32:32 +0700

    Thanks God

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from "react";
-import {Route, Routes } from "react-router-dom";
-import Home from "./Component/Pages/Home";
-import About from "./Component/Pages/About";
-import Contact from "./Component/Pages/Contact";
-import NavbarComp from './Component/Functional/NavbarComp'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+function App() {
+  const [articles, setArticles] = useState([]);
 
+  async function fetchArticlesHandler(){
+    const response = await fetch('http://localhost:8081');
+    let data = await response.json();
 
+    data = data => {
+        return {
+          Developer: data.Developer,
+          Project: data.Project
+        };
+    };
 
-function App () {
+    setArticles(data);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const response = await axios.get('http://localhost:8081');
+    setArticles(response.data);
+  };
+
   return (
-    <>
-    <NavbarComp />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about"element={<About />} />
-      <Route path="/contact"element={<Contact />} />
-    </Routes>
-    </>
-    
-  )
+    <div>{articles?.Developer}-{articles?.Project}</div>
+  );
 }
 
 export default App;
+
+
